4ff5cf3fa9c56298a03adf22b6247ec0dcf20514
Ходотчук Андрей <khodotchuk.andrew@gmail.com>
2026-04-16 20:57:48 +0200

    some changes

--- RevitTools.CeilingDiffuserElevation/CeilingDiffuserElevationCommand.cs
+++ RevitTools.CeilingDiffuserElevation/CeilingDiffuserElevationCommand.cs
@@ -24,7 +24,7 @@ namespace RevitTools.CeilingDiffuserElevation
 
 
                 // Создаём сервисы     
-                var ids = ElementIdLoaderHelper.LoadFromTxt(@"C:\Users\Khadatchuk\Downloads\ids.txt");
+                var ids = ElementIdLoaderHelper.LoadFromTxt(@"E:\makro\RevitTools\RevitTools.Core\Config\ids.txt");
                 var highlightService = new HighlightService(doc, ids);         
 
                 var diffuserService = new DiffuserService(doc);
